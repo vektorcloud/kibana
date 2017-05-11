d37e930c3c3e16d55c03aac1a6b6ce794441ccff
Bradley Cicenas <bradley@vektor.nyc>
2017-05-11 10:25:58 +0000

    read elastic host config from other vars

--- run.sh
+++ run.sh
@@ -1,10 +1,22 @@
 #!/bin/sh
 
-echo "server.host: 0.0.0.0" >> /kibana/config/kibana.yml
+es_hostvars="ES_HOST ELASTIC_URI"
 
-[ ! -z "$ES_HOST" ] && {
-  echo "elasticsearch.url: \"http://${ES_HOST}\"" >> /kibana/config/kibana.yml
+for var in $es_hostvars; do
+  [ ! -z "${!var}" ] && {
+    ES_HOST=${!var}
+    echo "configuring elastic host from env var $var ($ES_HOST)"
+    break
+  }
+done
+
+[ -z "$ES_HOST" ] && {
+  echo "elastic host must be defined in one of the following env vars: $es_hostvars"
+  exit 1
 }
+
+echo "server.host: 0.0.0.0" >> /kibana/config/kibana.yml
+echo "elasticsearch.url: \"http://${ES_HOST}\"" >> /kibana/config/kibana.yml
 
 cd /kibana 
 exec ./bin/kibana
